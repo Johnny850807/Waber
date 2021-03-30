9a900a54d53ac10766b8061158d536f6bc0e1508
johnny.pan <johnny.pan@linecorp.com>
2021-03-30 16:10:06 +0800

    :construction:

--- commons/src/main/java/tw/waterball/ddd/commons/utils/OpenTelemetryUtils.java
+++ commons/src/main/java/tw/waterball/ddd/commons/utils/OpenTelemetryUtils.java
@@ -1,13 +1,20 @@
 package tw.waterball.ddd.commons.utils;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+
 import io.opentelemetry.api.trace.Span;
 import lombok.Value;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * @author Waterball (johnny850807@gmail.com)
  */
 public class OpenTelemetryUtils {
-    public static void currentSpan(Configuration... configurations) {
+    public static AndThenLog currentSpan(Configuration... configurations) {
         Span span = Span.current();
 
         for (Configuration c : configurations) {
@@ -18,6 +25,8 @@ public class OpenTelemetryUtils {
                 span.addEvent(((Event) c).eventName);
             }
         }
+
+        return new AndThenLog(configurations);
     }
 
     public static Attr attr(Object key, Object val) {
@@ -39,5 +48,23 @@ public class OpenTelemetryUtils {
     @Value
     public static class Event implements Configuration {
         public String eventName;
+    }
+
+    public static class AndThenLog {
+        private final String message;
+
+        public AndThenLog(Configuration... configurations) {
+            String events = stream(configurations).filter(c -> c instanceof Event)
+                    .map(c -> ((Event) c).eventName).collect(joining(", "));
+            String attributes = stream(configurations).filter(c -> c instanceof Attr)
+                    .map(c -> (Attr) c)
+                    .map(attr -> attr.key + "=" + attr.val).collect(joining(", "));
+            message = String.format("%s%s", events.isBlank() ? "" : "events: [" + events + "] ",
+                    attributes.isBlank() ? "" : "attributes [" + attributes + "] ");
+        }
+
+        public void asLog(Consumer<String> messageConsumer) {
+            messageConsumer.accept(message);
+        }
     }
 }
--- spring-boot/spring-boot-user/src/main/java/tw/waterball/ddd/waber/springboot/user/controllers/DriverController.java
+++ spring-boot/spring-boot-user/src/main/java/tw/waterball/ddd/waber/springboot/user/controllers/DriverController.java
@@ -1,9 +1,15 @@
 package tw.waterball.ddd.waber.springboot.user.controllers;
 
+import static tw.waterball.ddd.commons.utils.OpenTelemetryUtils.attr;
+import static tw.waterball.ddd.commons.utils.OpenTelemetryUtils.currentSpan;
+import static tw.waterball.ddd.commons.utils.OpenTelemetryUtils.event;
+
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import tw.waterball.ddd.commons.model.ErrorMessage;
+import tw.waterball.ddd.commons.utils.OpenTelemetryUtils;
 import tw.waterball.ddd.model.user.Driver;
 import tw.waterball.ddd.model.user.DriverIsNotAvailableException;
 import tw.waterball.ddd.waber.driver.usecases.SetDriverStatus;
@@ -16,6 +22,7 @@ import java.util.Collection;
  * @author - johnny850807@gmail.com (Waterball)
  */
 @CrossOrigin
+@Slf4j
 @RequestMapping("/api/drivers")
 @RestController
 @AllArgsConstructor
@@ -26,6 +33,10 @@ public class DriverController {
 
     @PostMapping
     public Driver signUp(@RequestBody SignUpParams params) {
+        currentSpan(attr("name", params.name),
+                attr("email", params.name), attr("carType", params.carType))
+                .asLog(msg -> log.info("Sign up: {}", msg));
+
         return signUpToBeDriver.execute(
                 new SignUpToBeDriver.Request(params.name, params.email, params.password, params.carType)
         );
@@ -39,13 +50,17 @@ public class DriverController {
     @GetMapping
     public Collection<Driver> queryAvailableDrivers(@RequestParam(required = false) String activityName,
                                            @RequestParam(required = false) Driver.CarType carType) {
-        var drivers = findAvailableDrivers.execute(new FindAvailableDrivers.Request(activityName, carType));
-        return drivers;
+        currentSpan(attr("activityName", activityName), attr("carType", carType));
+
+        return findAvailableDrivers.execute(new FindAvailableDrivers.Request(activityName, carType));
     }
 
     @PatchMapping("/{driverId}")
     public void setDriverStatus(@PathVariable int driverId,
                                 @RequestBody String status) throws DriverIsNotAvailableException {
+        currentSpan(attr("driverId", driverId), attr("status", status))
+                .asLog(msg -> log.info("Set Driver Status: {}", msg));
+
         setDriverStatus.execute(new SetDriverStatus.Request(driverId,
                 Driver.Status.valueOf(status.trim().toUpperCase())));
     }
